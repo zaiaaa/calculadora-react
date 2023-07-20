7ae585f625b44448411472f27a227a416adac116
zaiaaa <gustavo.0801214@gmail.com>
2023-07-20 01:58:29 -0300

    commit

---
+++ src/components/Button/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+
+export const ButtonContainer = styled.button`
+    padding: 1.2rem;
+    border: 1px solid #CDCDCD;
+    background-color: #00AAF0;
+    color: #FFFFFF;
+    font-size: 1.625rem;
+    font-weight: 600;
+    flex: 1;
+    flex-wrap: wrap;
+
+    &:hover{
+        opacity: 0.8;
+    }
+
+`
---
+++ src/components/input/styles.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+
+    input {
+        width: 100%;
+        height: 100%;
+        background-color: #AAAAFF;
+        color: white;
+        border: 0;
+
+        font-size: 24px;
+        font-family: 'Roboto';
+
+        padding: 0 1rem;
+
+
+    }
+
+`
+
